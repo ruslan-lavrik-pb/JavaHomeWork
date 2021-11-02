e57fef8c0c966892f46f8e7952d4ba0bb0fb0f71
ruslan-lavrik-pb <92274591+ruslan-lavrik-pb@users.noreply.github.com>
2021-11-02 23:08:08 +0200

    init commit

---
+++ src/com/pb/lavrik/hw5/Reader.java
@@ -0,0 +1,50 @@
+package com.pb.lavrik.hw5;
+
+public class Reader {
+    private String fioName;
+    private String namberTiket;
+    private String fakyltetName;
+    private String dataHeppiBezd;
+    private String telefonNamder;
+
+    public String getFioName() {
+        return fioName;
+    }
+
+    public void setFioName(String fioName) {
+        this.fioName = fioName;
+    }
+
+    public String getNamberTiket() {
+        return namberTiket;
+    }
+
+    public void setNamberTiket(String namberTiket) {
+        this.namberTiket = namberTiket;
+    }
+
+    public String getFakyltetName() {
+        return fakyltetName;
+    }
+
+    public void setFakyltetName(String fakyltetName) {
+        this.fakyltetName = fakyltetName;
+    }
+
+    public String getDataHeppiBezd() {
+        return dataHeppiBezd;
+    }
+
+    public void setDataHeppiBezd(String dataHeppiBezd) {
+        this.dataHeppiBezd = dataHeppiBezd;
+    }
+
+    public String getTelefonNamder() {
+        return telefonNamder;
+    }
+
+    public void setTelefonNamder(String telefonNamder) {
+        this.telefonNamder = telefonNamder;
+    }
+}
+
