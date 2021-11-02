b8e682a603153214d0150b6cfcc330684d5cb515
ruslan-lavrik-pb <92274591+ruslan-lavrik-pb@users.noreply.github.com>
2021-11-02 23:07:13 +0200

    init commit

---
+++ src/com/pb/lavrik/hw5/Book.java
@@ -0,0 +1,30 @@
+package com.pb.lavrik.hw5;
+
+public class Book {
+    private String nazvaKnigi;
+    private String avtorKnigi;
+    private String godIzdanija;
+
+    public String getNazvaKnigi() {
+        return nazvaKnigi;
+    }
+
+    public String getAvtorKnigi() {
+        return avtorKnigi;
+    }
+
+    public String getGodIzdanija() {
+        return godIzdanija;
+    }
+
+    public void takeBook(String ... nazvaKnigis) {
+        for (String nazvaKnigi : nazvaKnigis){
+            nazvaKnigis.length
+
+        }
+
+
+
+    }
+
+}
