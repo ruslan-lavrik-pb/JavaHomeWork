0f6d82857b51f38cd37f6df06c1c6e1aab587a10
ruslan-lavrik-pb <92274591+ruslan-lavrik-pb@users.noreply.github.com>
2021-11-06 18:05:13 +0200

    init commit

---
+++ src/com/pb/lavrik/hw6/Animal.java
@@ -0,0 +1,49 @@
+package com.pb.lavrik.hw6;
+
+public class Animal {
+    private String name;
+    private String food;
+    private String location;
+
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void sleep() {
+        System.out.println("Животное: " + name + " спит");
+    }
+
+    public void eat() {
+        System.out.println("Животное " + name + "сейчас кушает");
+    }
+
+    public void makeNoise() {
+        System.out.println("Животное:" + name + "шумит");
+    }
+
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
