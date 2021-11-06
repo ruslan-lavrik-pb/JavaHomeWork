53781fb69c3e7565088e965d14397beea1b76a05
ruslan-lavrik-pb <92274591+ruslan-lavrik-pb@users.noreply.github.com>
2021-11-06 19:30:56 +0200

    init commit

--- src/com/pb/lavrik/hw6/Veterinarian .java
+++ src/com/pb/lavrik/hw6/Veterinarian .java
@@ -1,14 +1,28 @@
 package com.pb.lavrik.hw6;
 
+import java.util.List;
 
-
-public class Main {
-    private static Object treatAnimal;
+public class Veterinarian {
 
     public static void main(String[] args) {
         Cat cat1 = new Cat("Мурка", "Пятнистая");
-        Cat cat2 =new Cat("Мурка", "Пятнистая");
-        Cat cat3=new Cat("Барсик", "Пепельный");
+        Cat cat2 = new Cat("Мурка", "Пятнистая");
+        Cat cat3 = new Cat("Барсик", "Пепельный");
+        cat1.setLocation("Дом");
+        cat2.setLocation("Сарай");
+        cat3.setLocation("Дом");
+
+        Dog dog1 = new Dog("Барсик");
+        Dog dog2 = new Dog("Шакрик");
+        dog1.setLocation("Будка");
+        dog2.setLocation("Дом");
+
+        Horse horse = new Horse("Мустанг");
+        horse.setLocation("Сарай");
+
+        Animal[] lists = new Animal[]{cat1, cat2, cat3, dog1, dog2, horse};
+
+
         System.out.println(cat1.getName() + " - " + cat1.getColorCat());
         cat1.makeNoise();
         cat1.eat();
@@ -18,25 +32,28 @@ public class Main {
         horse1.sleep();
 
 
-        System.out.println("Сравнение трех котов - "+ cat1.equals(cat2));
-        System.out.println("Сравнение трех котов - "+ cat1.equals(cat3));
-        System.out.println("hashCode Кота - " + cat1.getName()+" - "+cat1.hashCode());
-        System.out.println("hashCode Кота - " + cat2.getName()+" - "+cat2.hashCode());
-        System.out.println("hashCode Кота - " + cat3.getName()+" - "+cat3.hashCode());
+        System.out.println("Сравнение трех котов - " + cat1.equals(cat2));
+        System.out.println("Сравнение трех котов - " + cat1.equals(cat3));
+        System.out.println("hashCode Кота - " + cat1.getName() + " - " + cat1.hashCode());
+        System.out.println("hashCode Кота - " + cat2.getName() + " - " + cat2.hashCode());
+        System.out.println("hashCode Кота - " + cat3.getName() + " - " + cat3.hashCode());
 
         System.out.println(cat1);
 
-
-    }
-
-
-    public class Veterinarian {
-        void treatAnimal(Animal animal){
-            System.out.println("на прием пришло животное "+animal);
+        for (Animal animal:lists) {
+            treatAnimal(animal);
         }
 
+
     }
 
+
+    static void treatAnimal(Animal animal) {
+        System.out.println("на прием пришло животное " + animal.getName() + " из - " +animal.getLocation());
+    }
+
+
+}
 
 
 }
