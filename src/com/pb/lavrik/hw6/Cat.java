05f4366589baaf21f0a943cd0c2bf509a2988435
ruslan-lavrik-pb <92274591+ruslan-lavrik-pb@users.noreply.github.com>
2021-11-06 18:06:48 +0200

    init commit

---
+++ src/com/pb/lavrik/hw6/Cat.java
@@ -0,0 +1,50 @@
+package com.pb.lavrik.hw6;
+
+import java.util.Objects;
+
+public class Cat extends Animal {
+
+    private String colorCat;
+
+    public Cat(String name, String colorCat) {
+        super(name);
+
+    }
+
+
+    public void makeNoise (){
+        System.out.println("Животное: " +getName()+ " шумит и царапает коврик, необходимо покормить");
+            }
+
+    public void eat (){
+        System.out.println("Животное: " + getName() + " сейчас кушает отбивную");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(colorCat, cat.colorCat);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(),colorCat);
+    }
+
+    public String getColorCat() {
+        return colorCat;
+    }
+
+    public void setColorCat(String colorCat) {
+        this.colorCat = colorCat;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "colorCat='" + colorCat + '\'' +
+                '}';
+    }
+}
