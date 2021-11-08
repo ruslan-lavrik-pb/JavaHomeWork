10f779eea938f05ab73fe4f624879639d6ced529
ruslan-lavrik-pb <92274591+ruslan-lavrik-pb@users.noreply.github.com>
2021-11-08 13:54:18 +0200

    init commit

--- src/com/pb/lavrik/hw6/Dog.java
+++ src/com/pb/lavrik/hw6/Dog.java
@@ -2,38 +2,55 @@ package com.pb.lavrik.hw6;
 
 import java.util.Objects;
 
-public class Dog extends Animal{
-    private String Poroda;
+public class Cat extends Animal {
 
-    public String getPoroda() {
-        return Poroda;
-    }
+    private String colorCat;
 
-    public void setPoroda(String poroda) {
-        Poroda = poroda;
-    }
-
-    public Dog(String name) {
+    public Cat(String name, String colorCat) {
         super(name);
+
+    }
+
+    public Cat() {
+
+    }
+
+
+    public void makeNoise() {
+        System.out.println("Животное: " + getName() + " шумит и царапает коврик, необходимо покормить");
+    }
+
+    public void eat() {
+        System.out.println("Животное: " + getName() + " сейчас кушает отбивную");
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Dog dog = (Dog) o;
-        return Objects.equals(Poroda, dog.Poroda);
+        Cat cat = (Cat) o;
+        return Objects.equals(colorCat, cat.colorCat);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName(),Poroda);
+        return Objects.hash(getName(), colorCat);
+    }
+
+    public String getColorCat() {
+        return colorCat;
+    }
+
+    public void setColorCat(String colorCat) {
+        this.colorCat = colorCat;
     }
 
     @Override
     public String toString() {
-        return "Dog{" +
+        return "Cat{" +
                 "Name='" + getName() + '\'' +
                 '}';
     }
+
+
 }
