8528ace36633c79b0dacbb40ffb31fbaadacc955
ruslan-lavrik-pb <92274591+ruslan-lavrik-pb@users.noreply.github.com>
2021-11-29 06:49:45 +0200

    init comet

--- src/com/pb/lavrik/hw10/NumBox.java
+++ src/com/pb/lavrik/hw10/NumBox.java
@@ -1,4 +1,18 @@
 package com.pb.lavrik.hw10;
 
-public class NumBox {
+public class NumBox <T extends Number> {
+
+    private T velichina;
+
+    public NumBox(T velichina) {
+        this.velichina = velichina;
+    }
+
+    public T getVelichina() {
+        return velichina;
+    }
+
+    public void setVelichina(T velichina) {
+        this.velichina = velichina;
+    }
 }
